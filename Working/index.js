afdd7b0c0608c1ed9ff587df145f0428adbd71a4
Jayanta garu <jayanta.garu@gmail.com>
2021-04-22 11:46:55 +0530

    js

---
+++ Working/index.js
@@ -0,0 +1,13 @@
+var x = 44;
+var y = 22;
+var x=y;
+console.log(x);}
+console.log(x==y);
+console.log()
+function avg(a, b){
+    c = (a+b)/2;
+    return c;
+}
+c1 = avg(4, 6);
+c2 = avg(14, 16);
+console.log(c1, c2);
